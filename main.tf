820fabda1ede541dc3269867a86fbd8aac4298c6
Ayaz6699 <ayaz6699@gmail.com>
2021-08-26 21:25:44 +1000

    Update main.tf
    Co-authored-by: Vladimir Mukhin <39741635+vladimirmukhin@users.noreply.github.com>

--- main.tf
+++ main.tf
@@ -96,6 +96,7 @@ resource "aws_route_table" "private" {
     Name = "private${count.index}"
   }
 }
+
 resource "aws_route_table_association" "private" {
   subnet_id      = aws_subnet.private.id
   route_table_id = aws_route_table.private.id
